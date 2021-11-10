7753bc8038e10a8b1b891e3bbd07552b2e82db08
Rzhavin Georgii <rzhavin@my.com>
2021-11-10 12:39:35 +0300

    Lesson 5

---
+++ pages/event/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Individual Event: #{{ this.$route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "event",
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about Event #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
